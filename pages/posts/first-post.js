2434b42e668060edadf2b9c1e783a29d55a878c0
Juan Bedoya <juan.bedoya@psl.com.co>
2021-04-25 15:47:28 -0500

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <span className='text-4xl text-gray-400'
+      >a test</span>
+    </Layout>
+  )
+}
